d943ff9ad372062c646df3f302bfab084661977e
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-04-24 03:18:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ import { fileURLToPath } from 'url';
 dotenv.config();
 
 // ESM: __filename i __dirname
-enable
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
